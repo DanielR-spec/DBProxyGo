41b99cd5e0d28b5a34ec48bdab5c9e1a4e873d37
daniel <daniel_reyes@javeriana.edu.co>
2023-12-09 02:40:44 -0500

    Initial command

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import "fmt"
-import "database/sql"
-import _ "go-sql-driver/mysql"
+//import "database/sql"
+//import _ "go-sql-driver/mysql"
 
-db, err := sql.Open("mysql", "motivy_user:53745344-e5dc-4406-a163-d765c3bf5ecf@motivy-prod-postgress.cgyidm3l3vnq.us-east-1.rds.amazonaws.com/motivyprod?parseTime=true")
+
 
 
 func main() {
-    fmt.Println("Database conexion test")
-    err := db.Ping()
+	//db, err = sql.Open("mysql", "")
+    fmt.Println("Hello World")
+    //err = db.Ping()
 }
