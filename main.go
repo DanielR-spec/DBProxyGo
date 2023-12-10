3c2d43a53b0849984dbe75b2c136da9d9978f79a
Daniel Reyes <daniel_reyes@javeriana.edu.co>
2023-12-10 10:52:17 -0500

    Feature/db conexion (#10)
    * Update
    >
    >
    Co-authored-by: Natalia <natikatkm1998@gmail.com>
    
    * Update README.md
    
    * conexion struct
    
    * Update main.go
    
    ---------
    
    Co-authored-by: Natk-Ktan <134225531+Natk-Ktan@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
-import "fmt"
-//import "database/sql"
-//import _ "go-sql-driver/mysql"
+import (
+    "database/sql"
+    "fmt"
+    "net/url"
+
+    _ "github.com/lib/pq"
+    _ "github.com/go-sql-driver/mysql"
+  )
 
 
-
+const (
+    host     = "host"
+    port     = 5432
+    user     = "user"
+    password = "password"
+    dbname   = "dbname"
+  )
 
 func main() {
-    //db, err = sql.Open("mysql", "")
-    //Initial hello world
-    fmt.Println("Hello World")
-    //err = db.Ping()
+
+    // Encoding the password
+    encodedPassword := url.QueryEscape(password)
+
+    // Constructing the connection string
+    connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=require", user, encodedPassword, host, port, dbname)
+    fmt.Println(connectionString)
+
+    // Connect to the database
+    db, err := sql.Open("postgres", connectionString)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    // Test the connection
+    err = db.Ping()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Successfully connected!")
+  
 }
